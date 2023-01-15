d2ab3696211f2ec5b66e3676d25f838e7fc29eb5
yb105 <51778416+yb105@users.noreply.github.com>
2023-01-15 15:41:55 +0530

    Delete Apps for portfolio directory

--- Apps for portfolio/Pizza And Pasta App/app/src/main/java/com/example/pizzaandpastaapp/Models/MyModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.pizzaandpastaapp.Models
-
-data class MyModel(var title:String, var image:Int, var price:String) {
-}
--- Apps for portfolio/Pizza And Pasta App/app/src/main/java/com/example/pizzaandpastaapp/Models/OrderModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.pizzaandpastaapp.Models
-
-data class OrderModel(var imageOrder:Int, var qty:String, var price:String) {
-}
