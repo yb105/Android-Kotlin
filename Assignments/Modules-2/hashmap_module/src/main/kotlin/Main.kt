4700452a913adb25f424c596be105af9a2b539f1
yb105 <51778416+yb105@users.noreply.github.com>
2023-02-04 15:24:59 +0530

    Delete Assignments directory

--- Assignments/Modules-2/hashmap_module/src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-
-    var capital: Map<String,String> = mapOf("Rajasthan" to "Jaipur", "Gujrat" to "Gandhinagar", "Maharasthra" to "Mumbai")
-
-    for (i in capital.keys){
-
-        println("$i   ${capital.get(i)}")
-    }
-}
