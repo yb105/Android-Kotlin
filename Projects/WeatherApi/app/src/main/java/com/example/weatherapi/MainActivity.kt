af1f7f1d75d28dd48df4e01534a37df88c897018
yb105 <10521yb@gmail.com>
2023-02-10 17:41:07 +0530

    up

--- Projects/WeatherApi/app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ Projects/WeatherApi/app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -4,7 +4,11 @@ import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.example.weatherapi.Models.Main
 import com.example.weatherapi.Models.Weather
 import com.example.weatherapi.Models.WeatherData
 import com.example.weatherapi.databinding.ActivityMainBinding
@@ -16,13 +20,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
      val apiId = "826448fdd6b73897fad1a91615d05983"
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-            getData()
+
+    binding.button.setOnClickListener {
+        if (binding.edplace.text.isEmpty()){
+            Toast.makeText(this, "Please enter city", Toast.LENGTH_SHORT).show()
+        }
+        getData()
+    }
+
 
 
     }
@@ -33,17 +47,19 @@ class MainActivity : AppCompatActivity() {
         progress.setMessage("wait till data is fetching")
         progress.show()
 
-        weatherService.weatherService.getWeatherData("mumbai",apiId).enqueue(object : Callback<WeatherData?> {
+        weatherService.weatherService.getWeatherData(binding.edplace.text.toString(),apiId).enqueue(object : Callback<WeatherData?> {
             override fun onResponse(call: Call<WeatherData?>, response: Response<WeatherData?>) {
                val data = response.body()
                 if(data!=null){
                    Log.d("hii",""+data)
 
                     var list: List<Weather>
-
                     list = data.weather
-                    
-                    binding.txt.text = list[0].description
+
+                    binding.descripion.text = list[0].description
+                    binding.main.text = list[0].main
+                    binding.temperature.text = ((data.main.temp - 273).toString()).substringBefore(".") + "°C"
+                    binding.cityname.text = data.name
 
                     progress.dismiss()
                 }
